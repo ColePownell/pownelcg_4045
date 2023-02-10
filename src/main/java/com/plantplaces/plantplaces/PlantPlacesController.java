d38b39f4c9e206122189c02095302abb11396868
Cole P. <pownelcg@mail.uc.edu>
2023-02-09 23:32:11 -0500

    Model push commit

--- src/main/java/com/plantplaces/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/plantplaces/PlantPlacesController.java
@@ -1,13 +1,50 @@
 package com.plantplaces.plantplaces;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
 
 @Controller
 public class PlantPlacesController {
 
+//	@RequestMapping(value = "/start", method=RequestMethod.GET)
+//	public String start()
+//	{
+//		return "start";
+//	}
+	
+	@RequestMapping(value = "/start", method=RequestMethod.GET)
+	public String read(Model model)
+	{
+		String test = "43 39.74 -84.51 A beautiful Eastern Redbud";
+		
+		model.addAttribute("specimenDTO", test);
+		return "start";
+	}
+	
+	@RequestMapping(value = "/start", method=RequestMethod.GET, params= {"loyalty=blue"})
+	public String readBlue()
+	{
+		return "start";
+	}
+	
+	@RequestMapping(value = "/start", method=RequestMethod.GET, params= {"loyalty=silver"})
+	public String readSilver()
+	{
+		return "start";
+	}
+	
+	@RequestMapping(value = "/start", method=RequestMethod.GET, headers= {"content-type=text/json"})
+	public String readJSON()
+	{
+		return "start";
+	}
+	
 	@RequestMapping("/start")
-	public String start()
+	public String create()
 	{
 		return "start";
 	}
